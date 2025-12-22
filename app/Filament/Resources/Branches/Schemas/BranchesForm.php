377dce077d6e4144f6cbcb903aa390b2be745ac0
TjayAmit <tristanjayamit0813@gmail.com>
2025-12-23 00:19:55 +0800

    feat: added branch form test creation and edit

--- app/Filament/Resources/Branches/Schemas/BranchesForm.php
+++ app/Filament/Resources/Branches/Schemas/BranchesForm.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Resources\Branches\Schemas;
 
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
 class BranchesForm
@@ -10,7 +12,16 @@ class BranchesForm
     {
         return $schema
             ->components([
-                //
+                TextInput::make('name'),
+                TextInput::make('address'),
+                TextInput::make('capacity_limit'),
+                TextInput::make('warning_threshold'),
+                Select::make('laundry_id')
+                    ->label('Laundry Shop')
+                    ->relationship('laundry', 'name')
+                    ->searchable()
+                    ->preload()
+                    ->required(),
             ]);
     }
 }
